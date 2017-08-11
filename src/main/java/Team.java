c0ca0f8b59f9ee6683ecfca6d20a5e3c6ad0005a
Kimberly Lu <kimlu4@comcast.net>
2017-08-11 08:37:35 -0700

    add constructor that takes in one argument so test can pass.

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,6 +1,21 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 /**
  * Created by Guest on 8/11/17.
  */
 public class Team {
+    private String name;
+    private String description;
+    private static ArrayList<Team> instances = new ArrayList<>();
+    private LocalDateTime createdAt;
+    private int id;
 
+    public Team (String name) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
 }
